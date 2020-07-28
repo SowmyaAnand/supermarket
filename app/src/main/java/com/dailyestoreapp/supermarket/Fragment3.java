63429f9fd6bcce440a2b1c38bd063378eba02e05
SowmyaAnand <sowmyaanand2509@gmail.com>
2020-07-28 15:16:46 +0530

    horizontal swiper

--- app/src/main/java/com/dailyestoreapp/supermarket/Fragment3.java
+++ app/src/main/java/com/dailyestoreapp/supermarket/Fragment3.java
@@ -1,21 +1,28 @@
 package com.dailyestoreapp.supermarket;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 public class Fragment3 extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+ImageView call,whats;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -58,41 +65,54 @@ EditText ed,ed2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final String number = "+919821182318";
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment3, container, false);
-        ed=(EditText)rootView.findViewById(R.id.edit_text);
-        ed2=(EditText)rootView.findViewById(R.id.edit_text2);
-        ed2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        call = (ImageView) rootView.findViewById(R.id.call);
+        whats = (ImageView) rootView.findViewById(R.id.whatsapp);
+        whats.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus)
-                {
-                    ed2.setHint("Enter your name");
+            public void onClick(View v) {
+                PackageManager pm=getContext().getPackageManager();
+                try {
+                    PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+                    Uri uri = Uri.parse("smsto:" + number);
+                    Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+                    i.setPackage("com.whatsapp");
+                    startActivity(Intent.createChooser(i, ""));
                 }
-                else
-                {
-                    ed2.setHint("");
+                catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(getContext(), "WhatsApp not Installed", Toast.LENGTH_SHORT)
+                            .show();
                 }
-            }
-        });
-        ed.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus)
-                {
-                 ed.setHint("Enter your name");
-                }
-                else
-                {
-                    ed.setHint("");
-                }
-            }
-        });
 
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 try {
+                   String ph = "+917025864742";
+                 Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + ph));
+                 startActivity(intent);
+                  } catch (Exception e) {
+                     Toast.makeText(getContext(),""+e,Toast.LENGTH_SHORT);
+                  }
+            }
+        });
 
         return rootView;
     }
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+    }
 
 }
 
